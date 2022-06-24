7d18dcac619dd3cb524a1b3db9256386881682a7
jordanss1 <jordan.shade@live.com>
2022-06-24 22:56:51 +0100

    initial commit for all files

---
+++ script.js
@@ -0,0 +1,4 @@
+//Create a Recipe App by integrating the "TheMealDB" API's random recipe endpoint. When the page first loads, it should 
+//display a random recipe, complete with a picture of the meal, name of the meal, a button to watch the YouTube video, and 
+//the instructions of how to make the meal, along with the list of ingredients. When a user clicks a button called "new recipe", 
+//it should show a new random and update the content on the page using JavaScript.
