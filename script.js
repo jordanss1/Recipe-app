d16203318808cf734114ca7e52e478fc8c541e05
jordanss1 <jordan.shade@live.com>
2022-06-25 02:12:48 +0100

    setting up html and style

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 //display a random recipe, complete with a picture of the meal, name of the meal, a button to watch the YouTube video, and 
 //the instructions of how to make the meal, along with the list of ingredients. When a user clicks a button called "new recipe", 
 //it should show a new random and update the content on the page using JavaScript.
+
+/*
+1. 
+
+*/
