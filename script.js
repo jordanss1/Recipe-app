c2b0f339208e4f8e3dfc30fab40d8f7d9bdf33ae
jordanss1 <jordan.shade@live.com>
2022-06-27 20:45:24 +0100

    working on filtering correct ingredients to new array

--- script.js
+++ script.js
@@ -8,28 +8,55 @@
 
 */
 
+
 const fetchApiAndDisplay = () => {
 	const mealName = document.getElementById("name"); 
 	const mealPicture = document.getElementById("first-color1");
 	const ingredients = document.getElementById("second-color1");
 	const instructions = document.getElementById("second-color2");
 	const youtube = document.getElementById("youtube");
+	const imgTag = document.createElement("img");
 
 	axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
 
 		.then(response => {
 			mealObject = response.data.meals[0];
-			console.log(mealObject);
-			mealName.innerHTML = mealObject.strMeal;
-			youtube
-			
-		})
+			let objToArray = Object.entries(mealObject);
 
+			if (mealObject.strYoutube === "") {
+				youtube.disabled = true;
+			}
+
+			console.log(mealObject);
+
+			mealName.innerHTML = mealObject.strMeal;
+
+			imgTag.setAttribute("src", mealObject.strMealThumb);
+			mealPicture.append(imgTag);
+
+			const regExpr = [/strIngredient/, /""/];
+			const newArray = objToArray.filter(element => {
+				return regExpr[0].test(element);
+			})
+			console.log(newArray);
+		})
 }
 
 window.onload = () => {
 	fetchApiAndDisplay();
 }
+
+document.getElementById("youtube").addEventListener("click", e => {
+	e.preventDefault();
+
+	axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
+
+		.then(response => {
+			let link = response.data.meals[0].strYoutube;
+			window.open(link, "_blank");
+		})
+
+})
 
 document.getElementById("recipe-btn").addEventListener("click", e => {
 	e.preventDefault();
